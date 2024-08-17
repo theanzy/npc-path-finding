474c71cc85084a024bfde7d281a00e559ff85e89
tzy <theanzy1@gmail.com>
2024-08-17 15:03:12 +0800

    setup window + asset

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,21 @@
 const rl = @import("raylib");
 const std = @import("std");
 
-pub fn main() anyerror!void {}
+pub fn main() !void {
+    const SCREEN_HEIGHT: i32 = 450;
+    const SCREEN_WIDTH: i32 = 800;
+
+    rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "path fiding");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+    while (!rl.windowShouldClose()) {
+        // update
+        // draw
+        rl.beginDrawing();
+        rl.clearBackground(rl.Color.ray_white);
+        rl.drawText("Congrats", 190, 200, 20, rl.Color.light_gray);
+
+        rl.endDrawing();
+    }
+}
