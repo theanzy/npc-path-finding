87e8e32b7145fa3f7b3d2d3e6f5d21b7c3a18d29
tzy <theanzy1@gmail.com>
2024-08-17 14:56:47 +0800

    initialize

---
+++ build.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const rlz = @import("raylib-zig");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
+    const exe = b.addExecutable(.{ .name = "npc-path-finding", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run");
+    run_step.dependOn(&run_cmd.step);
+
+    b.installArtifact(exe);
+}
---
+++ src/main.zig
@@ -0,0 +1,4 @@
+const rl = @import("raylib");
+const std = @import("std");
+
+pub fn main() anyerror!void {}
